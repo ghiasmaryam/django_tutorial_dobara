4b8a952d783fef24b67971f30cd3c7d8dade26fa
ghiasmaryam <ghiasmaryam09@gmail.com>
2023-07-14 09:37:04 +0500

    add_q_v

--- polls/urls.py
+++ polls/urls.py
@@ -4,9 +4,22 @@ from . import views
 
 app_name = "polls"
 
+
 urlpatterns = [
-   path("", views.IndexView.as_view(), name="index"),
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    # ex: /polls/
+    path("", views.index, name="index"),
+    # ex: /polls/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
+    # ex: /polls/5/choice/
+    path("<int:question_id>/choice/", views.choice, name="choice"),
+    # ex: /polls/5/vote_reset/
+    path("<int:question_id>/vote_reset/", views.vote_reset, name="vote_reset"),
+    # ex: /polls/new/
+    path("new/", views.add_question, name="new"),
+
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -8,23 +8,72 @@ from.models import Question,Choice
 
 
 
-class IndexView(generic.ListView):
-    template_name = "polls/index.html"
-    context_object_name = "latest_question_list"
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    data = {"question_list": latest_question_list,}
+    return HttpResponse(render(request, 'polls/index.html', data))
 
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = "polls/detail.html"
 
 
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = "polls/results.html"
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def choice(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    if request.method == 'GET':
+        return render(request, "polls/choice.html", {"question": question})
+    elif request.method == 'POST':
+        user_submitted_choice = request.POST["choice"]
+        if not user_submitted_choice:
+            return render(request, "polls/choice.html", {
+                "question": question,
+                "error_message": "Please enter a valid choice"
+            })
+
+        new_choice = Choice(
+            question=question,
+            choice_text=user_submitted_choice,
+        )
+        new_choice.save()
+        return HttpResponseRedirect(reverse("polls:detail", args=(question.id,)))
+
+
+def vote_reset(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    for choice in question.choice_set.all():
+        choice.votes = 0
+        choice.save()
+
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+def add_question(request):
+    if request.method == 'GET':
+        return render(request, "polls/new_question.html", {})
+    elif request.method == 'POST':
+        user_submitted_question = request.POST["question"]
+        if not user_submitted_question:
+            return render(request, "polls/new_question.html", {
+                "error_message": "Please enter a valid question"
+            })
+        
+        new_question = Question(
+            question_text=user_submitted_question,
+            pub_date=timezone.now(),
+        )
+        new_question.save()
+    return HttpResponseRedirect(reverse("polls:index",))
+
 
 
 def vote(request, question_id):
@@ -48,3 +97,5 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
+   
